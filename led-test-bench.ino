02e4815fd65044905e07d496100da2fa516fa7d9
Cooper Brislain <cbrislain@gmail.com>
2018-05-09 15:04:52 -0700

    added LCD support

--- led-test-bench.ino
+++ led-test-bench.ino
@@ -1,30 +1,61 @@
 #include "FastLED.h"
+#include <LiquidCrystal.h>
 
 #define IS_WS2812 0
 #define IS_APA102 1
 
-#define NUM_LEDS 153 // 5+153+5
+#define NUM_LEDS 200 // maximum ever; we won't use them all
 #define TEST_LEDS 5
 #define DATA_PIN 7
 #define CLOCK_PIN 6
 #define SPEED_PIN A0
 #define FRAME_DELAY 100
 #define BUTTON_PIN 4
+#define NUM_SPECS 3
 #define PI 3.1415
 //#define BRIGHTNESS_PIN 0
 //#define SPEED_PIN 1
 #define MIN_BRIGHTNESS 32
 #define MAX_BRIGHTNESS 255
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+
+int lcd_key     = 0;
+int adc_key_in  = 0;
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 CRGB leds[NUM_LEDS];
 
 int index;
-int index2;
+int spec_index;
 int count;
 int color;
 int brightness;
 int speed;
+int specs[NUM_SPECS];
+specs[0] = 52;
+specs[1] = 73;
+specs[2] = 143;
+
 int num_leds;
+
+int read_lcd_buttons() {
+    adc_key_in = analogRead(0);      // read the value from the sensor
+    // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+    // we add approx 50 to those values and check to see if we are close
+    if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+    if (adc_key_in < 50)   return btnRIGHT;
+    if (adc_key_in < 250)  return btnUP;
+    if (adc_key_in < 380)  return btnDOWN;
+    if (adc_key_in < 420)  return btnLEFT;
+    if (adc_key_in < 700)  return btnSELECT;
+    return btnNONE;  // when all others fail, return this...
+}
 
 void fade(){
     for(int i=0; i<NUM_LEDS; i++){
@@ -54,6 +85,9 @@ void fade_out_by(int b){
 }
 
 void setup() {
+    lcd.begin(16, 2);              // start the library
+    lcd.setCursor(0,0);
+    lcd.print("Velleman VMA203"); // print a simple message
     //Serial.begin(9600);
     if(IS_APA102) {
         FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR, DATA_RATE_MHZ(12)>(leds, NUM_LEDS);
@@ -66,10 +100,9 @@ void setup() {
     }
     FastLED.show();
     count=0;
-    num_leds = NUM_LEDS;
-    //randomSeed(analogRead(A3));
+    spec_index = 0;
+    num_leds = specs[spec_index];
     index = 0;
-    index2 = 0;
     speed = 0;
 }
 
@@ -151,7 +184,7 @@ void fill(CRGB color) {
     }
 }
 
-void light_tests(int num_leds) {
+void light_tests() {
     for(int i=0; i<TEST_LEDS-1; i++) {
         leds[i] = CRGB::Red;
         leds[TEST_LEDS+num_leds+TEST_LEDS-(i+1)] = CRGB::Blue;
@@ -162,7 +195,7 @@ void light_tests(int num_leds) {
     FastLED.show();
 }
 
-void test_strip(int num_leds) {
+void test_strip() {
     // assumes 5 LED test strip before, and after
     // num_leds is the number of LEDs in the strip to be tested
     for (int i=0; i<num_leds; i++) {
@@ -185,7 +218,7 @@ void test_strip(int num_leds) {
     delay(FRAME_DELAY);
 }
 
-void initialize(int num_leds) {
+void initialize() {
     fill(CRGB::Black);
     FastLED.show();
     count = 0;
@@ -193,9 +226,22 @@ void initialize(int num_leds) {
 }
 
 void loop() {
-    int num_leds = 143;
     initialize(num_leds);
     test_strip(num_leds);
+    if (int btn = read_lcd_buttons() != btnNONE) {
+        switch( btn ) {
+            case btnUP :
+                if(spec_index > 0) {
+                    spec_index--;
+                    num_leds = specs[spec_index];
+                }
+                break;
+            case btnDOWN :
+                spec_index = (spec_index + 1)%NUM_SPECS;
+                num_leds = specs[spec_index];
+                break;
+        }
+    }
     delay(FRAME_DELAY);
     count++;
 }
