fd7f149a35ffd2fce604b102220d6453d333cc05
Cooper Brislain <cbrislain@gmail.com>
2018-05-28 02:46:18 -0700

    removed lcd

--- led-test-bench.ino
+++ led-test-bench.ino
@@ -5,7 +5,7 @@
 #include <Dhcp.h>
 #include <PubSubClient.h>
 #include "FastLED.h"
-#include <LiquidCrystal.h>
+#include <ArduinoJson.h>
 
 #define IS_WS2812 0
 #define IS_APA102 1
@@ -18,9 +18,9 @@
 #define FRAME_DELAY 100
 #define BUTTON_PIN 4
 #define PI 3.1415
-#define BIG_BTN_COM 40
-#define BIG_BTN_NO 41
-#define BIG_BTN_LED 42
+#define BIG_BTN_COM 38
+#define BIG_BTN_NO 39
+#define BIG_BTN_LED 40
 //#define BRIGHTNESS_PIN 0
 //#define SPEED_PIN 1
 #define MIN_BRIGHTNESS 32
@@ -31,14 +31,15 @@
 #define btnLEFT   3
 #define btnSELECT 4
 #define btnNONE   5
+#define NUM_SPECS 5
+
+#define halt(s) { Serial.println(F( s )); while(1);  }
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length);
+void reconnect();
 
 int clock_ticking;
 int num_leds;
-
-int lcd_key     = 0;
-int adc_key_in  = 0;
-
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 CRGB leds[NUM_LEDS];
 
@@ -47,24 +48,19 @@ int spec_index;
 int count;
 int color;
 int brightness;
-int speed;
 bool btn_still_down;
 int specs[NUM_SPECS];
 
-int num_leds;
+// Ethernet Vars
+byte mac[] = { 0xCA, 0x3D, 0xB3, 0x33, 0xFC, 0x1D };
 
-int read_lcd_buttons() {
-    adc_key_in = analogRead(0);      // read the value from the sensor
-    // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
-    // we add approx 50 to those values and check to see if we are close
-    if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
-    if (adc_key_in < 50)   return btnRIGHT;
-    if (adc_key_in < 250)  return btnUP;
-    if (adc_key_in < 380)  return btnDOWN;
-    if (adc_key_in < 420)  return btnLEFT;
-    if (adc_key_in < 700)  return btnSELECT;
-    return btnNONE;  // when all others fail, return this...
-}
+const char* mqtt_server = "mqtt.spaceboycoop.com";
+const int mqtt_port = 1883;
+const char* mqtt_username = "spaceboycoop";
+const char* mqtt_key = "r7rObnC6i2paWPxeEMuWiF";
+
+EthernetClient eth_client;
+PubSubClient mqtt_client(eth_client);
 
 void fade(){
     for(int i=0; i<NUM_LEDS; i++){
@@ -93,14 +89,43 @@ void fade_out_by(int b){
     }
 }
 
+void progress() {
+    count++;
+    leds[count] = CRGB::DodgerBlue;
+    FastLED.show();
+}
+
+void failed() {
+    for(int i=0; i<count; i++) {
+        leds[i] = CRGB::Red;
+    }
+    FastLED.show();
+}
+
+void finish() {
+    for (int i=0; i<NUM_LEDS; i++) {
+        leds[i] = CRGB::Green;
+        FastLED.show();
+        delay(25);
+    }
+}
+
 void setup() {
-    lcd.begin(16, 2);              // start the library
-    lcd.setCursor(0,0);
-    lcd.print("LED Test Bench"); // print a simple message
-    specs[0] = 74;
-    specs[1] = 113;
-    specs[2] = 143;
-    //Serial.begin(9600);
+    Serial.begin(115200);
+    count=0;
+    index = 0;
+    specs[0] = 42;
+    specs[1] = 74;
+    specs[2] = 113;
+    specs[3] = 143;
+    specs[4] = 27;
+
+    spec_index = 0;
+    num_leds = specs[spec_index];
+
+    StaticJsonBuffer<200> jsonBuffer;
+
+    Serial.println("Starting LED Test Bench");
     if(IS_APA102) {
         FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR, DATA_RATE_MHZ(12)>(leds, NUM_LEDS);
     }
@@ -110,54 +135,41 @@ void setup() {
     for (int i=0; i<NUM_LEDS; i++) {
         leds[i] = CRGB::Black;
     }
+    leds[0] = CRGB::Green;
     FastLED.show();
-    count=0;
-    spec_index = 0;
-    num_leds = specs[spec_index];
-    index = 0;
-    speed = 0;
+    progress();
+    Serial.println("Initializing Interfaces");
+    digitalWrite(BIG_BTN_COM, HIGH);
+    digitalWrite(BIG_BTN_LED, LOW);
+    progress();
+    mqtt_client.setServer(mqtt_server, mqtt_port);
+    mqtt_client.setCallback(mqtt_callback);
+    progress();
+    Ethernet.begin(mac);
+
+    Serial.println(F("Connecting..."));
+    progress();
+    if(!Ethernet.begin(mac)) {
+        Serial.println(F("Ethernet configuration failed."));
+        failed();
+    } else {
+        progress();
+        Serial.println(F("Ethernet configured via DHCP"));
+        Serial.print("IP address: ");
+        Serial.println(Ethernet.localIP());
+        Serial.println();
+        progress();
+    }
+    finish();
+    delay(1500);
+    initialize();
 }
 
 void chase_white() {
-    for(int i=0; i<NUM_LEDS;i++){
-        if((int i+count)%10==0) {
+    for(int i=0; i<num_leds;i++){
+        if((i+count)%10==0) {
             leds[i] += CHSV(color+(((count+count/25)/10)%2*128)%255, 255, 85);
         }
-    }
-}
-
-void sinewave(){
-    for(int i=0;i<1000;i++){
-        fade();
-        leds[(num_leds+(int)(sin(i*PI/180)*num_leds/2+num_leds/2))%num_leds] = CRGB::White;
-        FastLED.show();
-        delay(10);
-    }
-}
-
-void randstrobe() {
-    for(int i=0; i<10000; i++) {
-        if(i%8==0) {
-            for(int j=random(NUM_LEDS); j<random(NUM_LEDS)+random(NUM_LEDS/3); j++) {
-                leds[j%NUM_LEDS] += CHSV(255,0,180);
-            }
-        }
-        FastLED.show();
-        fadeby(50);
-        count++;
-    }
-}
-
-void strobe() {
-    for(int i=0; i<10000; i++) {
-        if(i%4==0) {
-            int which = random(4);
-            for(i=which*143; i<which*143+143; i++) {
-                leds[i]+= CHSV(0,0,188);
-            }
-        }
-        FastLED.show();
-        count++;
     }
 }
 
@@ -167,84 +179,31 @@ void fill(CRGB color) {
     }
 }
 
-void light_tests() {
-    for(int i=0; i<TEST_LEDS-1; i++) {
-        leds[i] = CRGB::Red;
-        leds[TEST_LEDS+num_leds+TEST_LEDS-(i+1)] = CRGB::Blue;
-        FastLED.show();
-        delay(1000);
-        check_buttons();
-    }
-    fill(CRGB::Green);
-    FastLED.show();
-}
-
 void test_strip() {
-    // assumes 5 LED test strip before, and after
-    // num_leds is the number of LEDs in the strip to be tested
-    for (int i=0; i<num_leds; i++) {
-        CRGB color = CRGB::White;
-        int center = num_leds/2;
-        if (abs(i-center)%(i/3)==0) {
-            color = CRGB::Black;
-        }
-        leds[TEST_LEDS+i] = color;
-        leds[TEST_LEDS+num_leds-(i+1)] = color;
-        FastLED.show();
-        fadeby(25);
-        delay(3);
-        check_buttons();
+    for (int i=0; i<NUM_LEDS; i++) {
+        leds[i] = CRGB::Black;
     }
-    // as long as the first LED is still lit, keep fading out
-    // (others shoudl not be lit by the time this one fades out)
-    do {
-        fadeby(25);
-        FastLED.show();
-        delay(3);
-        check_buttons();
-    } while (leds[TEST_LEDS]);
-    delay(FRAME_DELAY);
+    leds[0] = CRGB::Red;
+    leds[num_leds-1] = CRGB::Red;
+    leds[num_leds] = CRGB::Blue;
+    for (int i=1; i<num_leds-1; i++) {
+        leds[i] = CRGB::Indigo;
+    }
+    FastLED.show();
+
+    delay(1000);
 }
 
 void initialize() {
     fill(CRGB::Black);
     FastLED.show();
     count = 0;
-    light_tests();
 }
 
-void print_status() {
+/*void print_status() {
     char buffer[16];
-    lcd.setCursor(0,0);
-    lcd.print("LED Test Bench");
-    lcd.setCursor(0,1);
     sprintf(buffer, "Spec %d - %d px", spec_index, num_leds);
-    lcd.print(buffer);
-}
-
-void check_buttons() {
-    int btn = read_lcd_buttons();
-    if(btn == btnNONE) {
-        btn_still_down = false;
-    } else {
-        if (!btn_still_down) {
-            btn_still_down = true;
-            switch( btn ) {
-                case btnUP :
-                    if(spec_index > 0) {
-                        spec_index--;
-                        num_leds = specs[spec_index];
-                    }
-                    break;
-                case btnDOWN :
-                    spec_index = (spec_index + 1)%NUM_SPECS;
-                    num_leds = specs[spec_index];
-                    break;
-            }
-            print_status();
-        }
-    }
-}
+}*/
 
 void check_big_button() {
     int val = digitalRead(BIG_BTN_NO);
@@ -259,12 +218,55 @@ void check_big_button() {
     }
 }
 
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    payload[length] = '\0';
+    if(strcmp(topic,"leds/test/checkin") == 0) {
+        Serial.println("Message received from leds/test/checkin");
+        int new_spec = atoi((char *)payload);
+        num_leds = specs[new_spec];
+        Serial.print("Chaned to spec ");
+        Serial.print(new_spec);
+        Serial.print('(');
+        Serial.print(num_leds);
+        Serial.print(')');
+    }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+    while (!mqtt_client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (mqtt_client.connect("arduinoClient",mqtt_username,mqtt_key)) {
+            Serial.println("connected");
+            // ... and resubscribe
+            if(!mqtt_client.subscribe("leds/test/checkin")) {
+                Serial.println("Subscribe to leds/test/checkin failed.");
+            } else {
+                Serial.println("Subscribed to leds/test/checkin.");
+            }
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(mqtt_client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+
 void loop() {
-    print_status();
-    initialize();
+    if (!mqtt_client.connected()) {
+        Serial.println("Reconnecting...");
+        reconnect();
+    }
+    mqtt_client.loop();
+    //print_status();
     test_strip();
     check_big_button();
-    check_buttons();
     delay(FRAME_DELAY);
     count++;
 }
