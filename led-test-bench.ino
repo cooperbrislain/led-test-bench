c5cead0a94a6dfd0d884805a0ce60ae530a5d843
Cooper Brislain <cbrislain@gmail.com>
2018-06-06 22:05:09 -0700

    working on this

--- led-test-bench.ino
+++ led-test-bench.ino
@@ -1,6 +1,7 @@
-#include "SPI.h"
 #include "FastLED.h"
 #include "LiquidCrystal.h"
+
+#define DEBUG 0
 
 #define IS_WS2812 0
 #define IS_APA102 1
@@ -38,76 +39,39 @@ int lcd_key     = 0;
 int adc_key_in  = 0;
 
 
-// read the buttons
 int read_LCD_buttons()
 {
- adc_key_in = analogRead(0);      // read the value from the sensor
- // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
- // we add approx 50 to those values and check to see if we are close
- if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
- // For V1.1 us this threshold
-
- if (adc_key_in < 50)   return btnRIGHT;
- if (adc_key_in < 250)  return btnUP;
- if (adc_key_in < 380)  return btnDOWN;
- if (adc_key_in < 420)  return btnLEFT;
- if (adc_key_in < 700)  return btnSELECT;
-
-
- // For V1.0 comment the other threshold and use the one below:
-/*
- if (adc_key_in < 50)   return btnRIGHT;
- if (adc_key_in < 195)  return btnUP;
- if (adc_key_in < 380)  return btnDOWN;
- if (adc_key_in < 555)  return btnLEFT;
- if (adc_key_in < 790)  return btnSELECT;
-*/
-
-
-    return btnNONE;  // when all others fail, return this...
+    adc_key_in = analogRead(0);
+    Serial.println(adc_key_in);
+    if (adc_key_in > 1000) return btnNONE;
+    if (adc_key_in < 50)   return btnRIGHT;
+    if (adc_key_in < 250)  return btnUP;
+    if (adc_key_in < 380)  return btnDOWN;
+    if (adc_key_in < 420)  return btnLEFT;
+    if (adc_key_in < 700)  return btnSELECT;
+    return btnNONE;
 }
 
 int num_leds;
 
 CRGB leds[NUM_LEDS];
 
-unsigned int mode;
-unsigned int index;
-unsigned int spec_index;
-unsigned int count;
-unsigned int hue;
-unsigned int brightness;
-unsigned int pressed;
-unsigned int pressed_time;
+unsigned int mode = 0;
+unsigned int index = 0;
+unsigned int spec_index = 0;
+unsigned int count = 0;
+unsigned int hue = 0;
+unsigned int brightness = 25;
+unsigned int pressed = btnNONE;
+unsigned int pressed_time = 0;
+
+unsigned int on = 0;
 
 unsigned int specs[NUM_SPECS];
-
-bool on;
 
 void fade(){
     for(int i=0; i<NUM_LEDS; i++){
          leds[i].fadeToBlackBy( 10 );
-    }
-}
-
-void countfade() {
-    for (int i=0;i<count;i++) {
-        if(i%10!=9) {
-            leds[i].fadeToBlackBy(10);
-        }
-    }
-}
-
-void fadeby(int b){
-    for(int i=0; i<NUM_LEDS; i++){
-         leds[i].fadeToBlackBy( b );
-    }
-}
-
-void fade_out_by(int b){
-    if(brightness > 0) {
-        brightness -= b;
-        FastLED.setBrightness(brightness);
     }
 }
 
@@ -129,16 +93,18 @@ void setup() {
     spec_index = 0;
     num_leds = specs[spec_index];
 
-    lcd.begin(16, 2);              // start the library
+    lcd.begin(16, 2);
     lcd.setCursor(0,0);
-    lcd.print("LED Field Tester"); // print a simple message
+    lcd.print("LED Field Tester");
     Serial.println("Starting LED Test Bench");
+
     if(IS_APA102) {
         FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR, DATA_RATE_MHZ(12)>(leds, NUM_LEDS);
     }
     if(IS_WS2812) {
         FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
     }
+
     initialize();
 }
 
@@ -148,17 +114,21 @@ void chase(CRGB color) {
 }
 
 void fill(CRGB color) {
-    for(int i=0; i<NUM_LEDS; i++) {
+    for(int i=0; i<num_leds; i++) {
         leds[i] = color;
     }
 }
 
 void turn_on() {
-
+    on = 1;
+    lcd.setCursor(0,1);
+    lcd.print("On       ");
 }
 
 void turn_off() {
-
+    on = 0;
+    lcd.setCursor(0,1);
+    lcd.print("Off       ");
 }
 
 void test_strip() {
@@ -172,20 +142,14 @@ void test_strip() {
 
 void initialize() {
     for (int i=0; i<NUM_LEDS; i++) {
-        fade();
-        leds[i] = CRGB::White;
-        FastLED.show();
-    }
-    while(leds[NUM_LEDS-1] != CRGB(0,0,0)) {
-        fade();
-        FastLED.show();
+        leds[i] = CRGB::Black;
     }
     FastLED.show();
     count = 0;
 }
 
 void blink_end() {
-    if (num_leds != NUM_LEDS) {
+    if (num_leds < NUM_LEDS) {
         if (count%25 == 0) {
             if (leds[num_leds] != CRGB(0,0,0)) {
                 leds[num_leds] = CRGB::Black;
@@ -196,44 +160,90 @@ void blink_end() {
     }
 }
 
+String get_button_name(int the_key) {
+    switch (the_key) {
+        case btnNONE:
+            return "None";
+        case btnUP:
+            return "Up";
+        case btnDOWN:
+            return "Down";
+        case btnLEFT:
+            return "Left";
+        case btnSELECT:
+            return "Select";
+        case btnRIGHT:
+            return "Right";
+        default:
+            // this should never happen
+            break;
+    }
+}
+
 void loop() {
-    int lcd_key = read_LCD_buttons();
-    Serial.println(lcd_key);
     char buffer[4];
-    if (pressed == lcd_key && lcd_key != btnNONE) {
-        Serial.print("still pressed for ");
-        Serial.print(pressed_time);
-        Serial.println(" seconds");
-        pressed_time++;
-    } else if (lcd_key == btnNONE && pressed != btnNONE) {
-        Serial.print("released after ");
-        Serial.print(pressed_time);
-        Serial.println(" seconds");
-    } else if (lcd_key != btnNONE && pressed == btnNONE) {
-        Serial.println("pressed");
+    int lcd_key = read_LCD_buttons();
+    if (lcd_key != pressed) {
+        if (DEBUG && lcd_key != btnNONE) {
+            Serial.print(get_button_name(lcd_key));
+            Serial.println(" pressed");
+        }
+        if (lcd_key == btnNONE) {
+            if (DEBUG) {
+                Serial.print(get_button_name(pressed));
+                Serial.print(" released after ");
+                Serial.println(pressed_time);
+            }
+            if (pressed_time <= 3) {
+                switch (pressed) {
+                    case btnUP:
+                        if (!on) {
+                            turn_on();
+                        }
+                        break;
+                    case btnDOWN:
+                        if (on) {
+                            turn_off();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
         pressed = lcd_key;
         pressed_time = 0;
-    }
-    if (pressed == lcd_key && pressed_time > 3) {
-        Serial.println("long press");
-        if (pressed == lcd_key && pressed_time > 3) {
+    } else if (lcd_key != btnNONE) {
+        pressed_time++;
+        if (DEBUG) {
+            Serial.print("still pressed for ");
+            Serial.println(pressed_time);
+        }
+        if (pressed_time > 3) {
             switch (pressed) {
                 case btnUP:
-                    hue++;
-                    lcd.setCursor(0,1);
-                    lcd.print("Hue ");
-                    lcd.print(" - ");
-                    lcd.print(hue);
-                    break;
-                case btnDOWN:
-                    hue--;
-                    if (hue < 255) {
-                        hue= 0;
+                    if (!on) {
+                        turn_on();
+                        brightness = 0;
+                    }
+                    if (brightness < 255) {
+                        brightness++;
                     }
                     lcd.setCursor(0,1);
-                    lcd.print("Hue ");
+                    lcd.print("Bright");
                     lcd.print(" - ");
-                    lcd.print(hue);
+                    lcd.println(brightness);
+                    break;
+                case btnDOWN:
+                    if (hue > 0) {
+                        hue--;
+                    } else {
+                        turn_off();
+                    }
+                    lcd.setCursor(0,1);
+                    lcd.print("Bright");
+                    lcd.print(" - ");
+                    lcd.println(brightness);
                     break;
                 case btnLEFT:
                     spec_index++;
@@ -246,54 +256,39 @@ void loop() {
                     lcd.print(spec_index);
                     lcd.print(" - ");
                     lcd.print(num_leds);
-                    lcd.print("px");
+                    lcd.println("px");
                     break;
             }
         }
-        pressed_time++;
+    } else if (lcd_key == btnNONE) {
+        pressed = btnNONE;
+        pressed_time = 0;
     }
-    if (!pressed && lcd_key != btnNONE) {
-        if (pressed_time < 3) { // short press on release
-            switch (pressed) {
-                case btnUP:
-                    if (!on) {
-                        on = 1;
-                        lcd.setCursor(0,1);
-                        lcd.print("On");
-                    }
-                    break;
-                case btnDOWN:
-                    if (on) {
-                        on = 0;
-                        lcd.setCursor(0,1);
-                        lcd.print("Off");
-                    }
-                    break;
-                default:
-                    break;
-            }
+    // led animations
+    if (on) {
+        switch (mode) {
+            case CHASE_WHITE:
+                chase(CHSV(0,0,brightness));
+                break;
+            case FILL_WHITE:
+                fill(CHSV(0,0,brightness));
+                break;
+            case CHASE_COLOR:
+                chase(CHSV(hue,255,brightness));
+                break;
+            case FILL_COLOR:
+                fill(CHSV(hue,255,brightness));
+                break;
+            case TEST_STRIP:
+                test_strip();
+                break;
         }
-    }
-    switch (mode) {
-        case CHASE_WHITE:
-            chase(CRGB::White);
-            break;
-        case FILL_WHITE:
-            fill(CRGB::White);
-            break;
-        case CHASE_COLOR:
-            chase(CHSV(hue,255,255));
-            break;
-        case FILL_COLOR:
-            fill(CHSV(hue,255,255));
-            break;
-        case TEST_STRIP:
-            test_strip();
-            break;
+    } else {
+        fill(CRGB::Black);
     }
     FastLED.show();
     count++;
-    delay(FRAME_DELAY);
+    //delay(FRAME_DELAY);
 }
 
 
